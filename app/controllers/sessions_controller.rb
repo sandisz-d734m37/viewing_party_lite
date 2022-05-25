dfc6bfa3e1b05bfa34620972283ce693f53aa98b
Sandisz Thieme <sandiszthieme@gmail.com>
2022-05-24 18:00:24 -0600

    Add logout button to welcome page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,6 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
-      # binding.pry
       session[:user_id] = user.id
       redirect_to "/"
       flash[:success] = "Welcome, #{user.email}"
